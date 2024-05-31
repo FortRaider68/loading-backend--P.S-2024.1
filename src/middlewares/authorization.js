7f0ffb021d10f704be2aa04cc2397ce9ecc5645f
Pedro Rickson <pedrorickson95.fa@gmail.com>
2024-05-31 03:31:19 -0300

    Adding verification for token not provided

--- src/middlewares/authorization.js
+++ src/middlewares/authorization.js
@@ -3,6 +3,9 @@ const jwt = require("jsonwebtoken");
 module.exports = async(req,res,next) => {
     const {token} = req.cookies;
 
+    if(!token)
+        return res.send({error:"Token not provided"});
+
     const user = jwt.verify(token,process.env.SECRET_JWT_HASH);
 
     if(!user)
